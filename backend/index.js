7fa5f383cbf93455ef871d913aaa36b0665d3da2
Pranav <pranavangrishmusic@gmail.com>
2024-07-08 14:57:09 +0530

    localhost

---
+++ backend/index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRouter = require('./routes/user_routes');
+const authRouter = require('./routes/authRoutes');
+const cors = require('cors');
+const path = require('path');
+const app = express()
+app.use(cors({
+  origin: 'https://midorchard-client.vercel.app', // Adjust this to your frontend URL
+  // origin: *
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", 'https://midorchard-client.vercel.app');
+  res.header("Access-Control-Allow-Methods", ['GET', 'POST', 'PUT', 'DELETE']);
+  res.header("Access-Control-Allow-Headers",['Content-Type', 'Authorization']);
+  next();
+});
+
+
+const port = process.env.PORT || 3000
+const cleanupUnverifiedUsers = require('./Utils/cleanupUnverifiedUsers'); 
+const formRoutes = require('./routes/formSubmit');
+dotenv.config();
+
+app.use(express.json());
+
+
+
+
+app.options('*', cors());
+
+
+mongoose.connect(
+    process.env.MONGODB
+).then(()=>{
+    console.log('Connected to the database');
+    cleanupUnverifiedUsers();
+}
+    
+).catch((e)=>{
+    console.log('Error connecting to the database');
+    console.log(e);
+})
+
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+
+app.use('/api', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/forms', formRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!!`)
+})
